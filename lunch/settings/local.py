d6bc6f7f5a4b572c6faacafeea8799333df2ab42
casper chang <casper_chang@wistron.com>
2017-06-15 17:33:44 +0800

    migrate Models (Day07 of 30days)

--- lunch/settings/local.py
+++ lunch/settings/local.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'qdekc@z$t&x(!npwz3_a+i24-h4@00&%0zyskokv*=o-ur-@%*'
 DEBUG = True
-TEMPLATE_DEBUG = True
+#TEMPLATE_DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
